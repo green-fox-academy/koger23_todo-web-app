ddc5d3954b51c4ba534e7fd030b8fa51c57d1622
Gergely Kocsis <koger23@gmail.com>
2019-05-29 12:33:50 +0200

    fix(TodoService): fix add() with adding Date() objects

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/service/TodoService.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/service/TodoService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +31,8 @@ public class TodoService {
   }
 
   public void add(Todo todo) {
+    todo.setDate(new Date());
+    todo.setTime(new Date());
     todoRepository.save(todo);
   }
 
