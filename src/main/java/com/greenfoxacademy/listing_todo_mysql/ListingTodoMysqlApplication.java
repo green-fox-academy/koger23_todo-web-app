a0bdcba6525c70d7c72dd2f641d93ee8fa8ccacb
Gergely Kocsis <koger23@gmail.com>
2019-05-30 11:57:35 +0200

    refactor(TodoRepository): rename to ITodoRepository

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/ListingTodoMysqlApplication.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/ListingTodoMysqlApplication.java
@@ -1,17 +1,16 @@
 package com.greenfoxacademy.listing_todo_mysql;
 
-import com.greenfoxacademy.listing_todo_mysql.model.Todo;
-import com.greenfoxacademy.listing_todo_mysql.repository.TodoRepository;
+import com.greenfoxacademy.listing_todo_mysql.repository.ITodoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListingTodoMysqlApplication implements CommandLineRunner {
-  private TodoRepository todoRepository;
+  private ITodoRepository ITodoRepository;
 
-  public ListingTodoMysqlApplication(TodoRepository todoRepository) {
-    this.todoRepository = todoRepository;
+  public ListingTodoMysqlApplication(ITodoRepository ITodoRepository) {
+    this.ITodoRepository = ITodoRepository;
   }
 
   public static void main(String[] args) {
--- src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/ITodoRepository.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/ITodoRepository.java
@@ -3,5 +3,5 @@ package com.greenfoxacademy.listing_todo_mysql.repository;
 import com.greenfoxacademy.listing_todo_mysql.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodoRepository extends CrudRepository<Todo, Long> {
+public interface ITodoRepository extends CrudRepository<Todo, Long> {
 }
