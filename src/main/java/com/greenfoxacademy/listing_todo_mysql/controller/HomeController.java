c4cfae610ab94baaed054159643fc352add1d569
Gergely Kocsis <koger23@gmail.com>
2019-05-30 17:05:49 +0200

    refactor(HomeController): optimized imports

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/controller/HomeController.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/controller/HomeController.java
@@ -1,21 +1,22 @@
 package com.greenfoxacademy.listing_todo_mysql.controller;
 
+import com.greenfoxacademy.listing_todo_mysql.service.ITodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.greenfoxacademy.listing_todo_mysql.service.TodoService;
+import com.greenfoxacademy.listing_todo_mysql.service.TodoServiceImp;
 
 @Controller
 public class HomeController {
-  private TodoService todoService;
+  private ITodoService todoServiceImp;
 
-  public HomeController(TodoService todoService) {
-    this.todoService = todoService;
+  public HomeController(TodoServiceImp todoServiceImp) {
+    this.todoServiceImp = todoServiceImp;
   }
 
   @GetMapping("/")
   public String list(Model model) {
-    model.addAttribute("todolist", todoService.findAll());
+    model.addAttribute("todolist", todoServiceImp.findAll());
     return "todo";
   }
 }
