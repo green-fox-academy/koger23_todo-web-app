3e932af77aa7440456852300ce37d79d0a3068a9
Gergely Kocsis <koger23@gmail.com>
2019-05-30 17:07:02 +0200

    feat(ITodoRepository): add custom query

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/ITodoRepository.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/ITodoRepository.java
@@ -1,7 +1,12 @@
 package com.greenfoxacademy.listing_todo_mysql.repository;
 
 import com.greenfoxacademy.listing_todo_mysql.model.Todo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ITodoRepository extends CrudRepository<Todo, Long> {
+  @Query("select t from Todo t where t.title LIKE CONCAT('%',?1,'%')")
+  List<Todo> findAllByTitleContains(String keyword);
 }
