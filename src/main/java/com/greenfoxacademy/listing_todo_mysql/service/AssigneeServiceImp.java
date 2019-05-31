5d9382e17ab4b6cb4dda56d5b3171575ea4b8882
Gergely Kocsis <koger23@gmail.com>
2019-05-31 21:59:50 +0200

    feat(AssigneeServiceImp): add new methods

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/service/AssigneeServiceImp.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/service/AssigneeServiceImp.java
@@ -26,4 +26,36 @@ public class AssigneeServiceImp implements IAssigneeService {
     assigneeRepository.findAll().forEach(assigneeList::add);
     return assigneeList;
   }
+
+  @Override
+  public void add(Assignee assignee) {
+    assigneeRepository.save(assignee);
+  }
+
+  @Override
+  public void delete(long id) {
+    assigneeRepository.deleteById(id);
+  }
+
+  @Override
+  public Assignee findById(long id) {
+    return assigneeRepository.findById(id).orElse(new Assignee());
+  }
+
+  @Override
+  public void update(long id, Assignee newAssignee, String firstName, String lastName) {
+    Assignee assignee = assigneeRepository.findByFirstNameAndLastName(firstName, lastName);
+    assignee.setFirstName(newAssignee.getFirstName());
+    assignee.setLastName(newAssignee.getLastName());
+    assigneeRepository.save(assignee);
+  }
+
+  @Override
+  public List<Assignee> search(String keyword) {
+    return assigneeRepository.findAllByFirstNameIsLikeAnOrLastNameIsLike(keyword);
+  }
+
+  public Assignee findAllByFirstNameAndLastNameAndEmail(String firstName, String lastName, String email) {
+    return assigneeRepository.findAllByFirstNameAndLastNameAndEmail(firstName, lastName, email);
+  }
 }
