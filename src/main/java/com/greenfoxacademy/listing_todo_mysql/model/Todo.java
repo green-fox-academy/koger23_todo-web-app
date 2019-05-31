a756be629b5887d27e9c0f0eece7e354d69dff64
Gergely Kocsis <koger23@gmail.com>
2019-05-31 21:46:23 +0200

    feat(Todo): add due date

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/model/Todo.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/model/Todo.java
@@ -1,14 +1,11 @@
 package com.greenfoxacademy.listing_todo_mysql.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Getter
-@Setter
 public class Todo {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,6 +22,8 @@ public class Todo {
   @Column(nullable = false, name = "Created_at")
   @Temporal(TemporalType.TIME)
   private Date time;
+  @Column(name = "dueDate")
+  private java.sql.Date dueDate;
   @ManyToOne(fetch = FetchType.LAZY)
   private Assignee assignee;
 
@@ -32,15 +31,80 @@ public class Todo {
   }
 
   public Todo(String title) {
-    this(title, false, false, new Date(), new Date(), new Assignee());
+    this(title, false, false, new Date(), new Date(), null, new Assignee());
   }
 
-  public Todo(String title, boolean urgent, boolean done, Date date, Date time, Assignee assignee) {
+  public Todo(String title, boolean urgent, boolean done, Date date, Date time, java.sql.Date dueDate, Assignee assignee) {
     this.title = title;
     this.urgent = urgent;
     this.done = done;
     this.date = date;
     this.time = time;
+    this.dueDate = dueDate;
+    this.assignee = assignee;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public Date getTime() {
+    return time;
+  }
+
+  public void setTime(Date time) {
+    this.time = time;
+  }
+
+  public java.sql.Date getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(@DateTimeFormat(pattern = "yyyy-MM-dd") java.sql.Date date) {
+    this.dueDate = date;
+  }
+
+  public Assignee getAssignee() {
+    return assignee;
+  }
+
+  public void setAssignee(Assignee assignee) {
     this.assignee = assignee;
   }
 }
