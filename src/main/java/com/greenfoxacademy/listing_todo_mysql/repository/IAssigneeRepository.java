850b4a766c4619325c9cf500c1dd2e74235c964c
Gergely Kocsis <koger23@gmail.com>
2019-05-31 21:55:48 +0200

    feat(IAssigneeService, AssigneeImp): add new methods

--- src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/IAssigneeRepository.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/repository/IAssigneeRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 public interface IAssigneeRepository extends CrudRepository<Assignee, Long> {
   Assignee findByFirstNameAndLastName(String first_name, String last_name);
-  @Query("select a from Assignee a where a.lastName LIKE CONCAT('%',?1,'%') or a.firstName LIKE CONCAT('%',?1,'%')")
-  List<Assignee> findAllThatContains(String keyword);
+
+  @Query("select a from Assignee a where a.lastName LIKE CONCAT('%',?1,'%') or a.firstName LIKE CONCAT('%',?1,'%') or a.email LIKE CONCAT('%',?1,'%')")
+  List<Assignee> findAllByFirstNameIsLikeAnOrLastNameIsLike(String keyword);
+
+  @Query("select a from Assignee a where a.lastName=?1 and a.firstName=?2 and a.email=?3")
+  Assignee findAllByFirstNameAndLastNameAndEmail(String firstName, String lastName, String email);
 }
--- src/main/java/com/greenfoxacademy/listing_todo_mysql/service/IAssigneeService.java
+++ src/main/java/com/greenfoxacademy/listing_todo_mysql/service/IAssigneeService.java
@@ -5,6 +5,19 @@ import com.greenfoxacademy.listing_todo_mysql.model.Assignee;
 import java.util.List;
 
 public interface IAssigneeService {
-  void save(Assignee assignee);
   List<Assignee> findAll();
+
+  void add(Assignee assignee);
+
+  void delete(long id);
+
+  Assignee findById(long id);
+
+  void update(long id, Assignee newAssignee, String firstname, String lastname);
+
+  List<Assignee> search(String keyword);
+
+  void save(Assignee assignee);
+
+  Assignee findAllByFirstNameAndLastNameAndEmail(String firstName, String lastName, String email);
 }
